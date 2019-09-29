b7ce5a3ae3db742cfb425f8217adc06f8117137c
jarey <jose.angel.rey.linares@gmail.com>
2019-09-29 23:58:19 +0200

    La implementación actual no sirve, descarga más bytes que los que de verdad ocupa el fichero referencia y lo corrompe en cliente. Resolver: envio de cabeceras Range. Envío al cliente de la response con el número correcto de bytes y contenido sin corromper.

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -10,23 +10,76 @@ import { saveAs } from 'file-saver';
 })
 export class AppComponent {
 
+  /**
+   * 
+   * La idea es construir un componente que:
+   * 1) Mediante HEAD obtenga los datos del fichero a descargar: 1- tamaño (importantisimo), 2)nombre y extensión.
+   * 2) Con esa información componer una estrategia donde mediante N llamadas http, se soliciten parte por parte los chunks del fichero, se obtengan en el servidor y 
+   * se descarguen al cliente.
+   * 3) Una vez descargados todos los chunks, se unan en el fichero resultante y se descargue dicho fichero al equipo del usuario.
+   * 
+   * La aproximación de este POC es trabajar inicialmente con fileSaver, pero luego permutar al uso de StreamSaver para poder gestionar tamaños de fichero mucho más elevados
+   * sin hacer uso directo de RAM y por tanto evitando potenciales errores de memoria y carga del navegador web.
+   * 
+   * El comportamiento del backend, al igual que en la subida de ficheros de TUS, tiene que estar alineado con el comportamiento del cliente.
+   * 
+   * El comportamiento inicial del POC, está orientado a ampliar al máximo el soporte de navegadores. Después se podría complicar en el componente,
+   * bien con polyfills o con detección de navegadores en cuestión, para hacer uso por ejemplo del API de Streams, artículo de introducción/referencia a ello:
+   * https://jakearchibald.com/2016/streams-ftw/
+   * 
+   * Profundizar en si el siguiente proyecto ya incluye o aborda lo comentado anteriormente:
+   * https://github.com/alferov/angular-file-saver
+   * 
+   * Otra referencia similar a este POC, trabajando en componente con octec-stream: http://amilspage.com/angular4-file-download/
+   * 
+   * Uso de la header range para solicitar chunks de fichero: https://stackoverflow.com/questions/23309841/download-file-in-chunks-in-chrome-javascript-api
+   * https://stackoverflow.com/questions/14438187/javascript-filereader-parsing-long-file-in-chunks
+   * 
+   * Soporte a requests mediante rango de bytes en spring mvc: https://stackoverflow.com/questions/28427339/how-to-implement-http-byte-range-requests-in-spring-mvc
+   * 
+   */
+
   constructor(private http: HttpClient) { }
 
   saveFile() {
     let headers = new HttpHeaders()
     headers.append('Accept', 'text/plain');
-    this.http.get('http://localhost:8080/api/files', {headers: headers, responseType: 'text'})
-      .toPromise()
-      .then(response => this.saveToFileSystem(response));
+
+    this.http.head('http://localhost:8080/api/files', {headers: headers, responseType: 'text', observe: 'response'})
+    .toPromise()
+    .then(response => this.headInfo(response));
+
+
+    
   }
 
   private saveToFileSystem(response: any) {
-    //const contentDispositionHeader: string = response.headers.get('Content-Disposition');
-    //const parts: string[] = contentDispositionHeader.split(';');
-    //const filename = parts[1].split('=')[1];
-    //const blob = new Blob([response._body], { type: 'text/plain' });
-    const blob = new Blob([response], { type: 'text/plain' });
-    const filename = "MiFichero.txt";
+    console.log("El valor obtenido es: "+response.body);
+    const contentDispositionHeader: string = response.headers.get('Content-Disposition');
+    const parts: string[] = contentDispositionHeader.split(';');
+    const filename = parts[1].split('=')[1];
+    console.log("Nombre del fichero: "+ filename);
+    const blob = new Blob([response.body], { type: 'audio/mpeg3' });
     saveAs(blob, filename);
+  }
+
+  private headInfo(response: any) {
+
+    let headers = new HttpHeaders()
+    headers.append('Accept', 'text/plain');
+    
+
+    console.log("El valor obtenido es: "+response.body);
+    const contentDispositionHeader: string = response.headers.get('Content-Disposition');
+    const contentLength: string = response.headers.get('Content-Length');
+    headers.append('Range', "bytes=0-"+contentLength);
+    const parts: string[] = contentDispositionHeader.split(';');
+    const filename = parts[1].split('=')[1];
+    console.log("Nombre del fichero: "+ filename);
+    console.log("Tamaño del fichero: "+ contentLength);
+
+    this.http.get('http://localhost:8080/api/files', {headers: headers, responseType: 'text', observe: 'response'})
+      .toPromise()
+      .then(response => this.saveToFileSystem(response));
   }
 }
--- spring-boot-backend/src/main/java/com/example/filesavebackend/Application.java
+++ spring-boot-backend/src/main/java/com/example/filesavebackend/Application.java
@@ -1,16 +1,26 @@
 package com.example.filesavebackend;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -40,16 +50,53 @@ public class Application {
 @RestController
 class FileResource {
 
+	/**
+	 * Metodo del POC para descargar chunks del fichero.
+	 * @return
+	 */
     @GetMapping(path = "/api/files", produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<String> getFile() {
+    public ResponseEntity<byte[]> getFile(HttpServletRequest request) {
+    	String range = request.getHeader("Range");
+    	//String[] rangeElements = range.split("-");
+    	//Integer rangeFrom = Integer.valueOf(rangeElements[0]);
+    	//Integer rangeTo = Integer.valueOf(rangeElements[1]);
+    	Integer rangeFrom = 0;
+    	Integer rangeTo = 20224833;
+    	
+    	
         String exportedContent = "Hello, World!";
-        String filename = "my-file.txt";
-        HttpHeaders headers = new HttpHeaders();
-        InputStream inputStream = getClass()
-    			.getClassLoader().getResourceAsStream("Joseph Ray - Room 1.5 (Extended Mix).mp3");
-        headers.setAccessControlExposeHeaders(Collections.singletonList("Content-Disposition"));
-        headers.set("Content-Disposition", "attachment; filename=" + filename);
-        return new ResponseEntity<>(exportedContent, headers, HttpStatus.OK);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        InputStream inputStream = null;
+        byte[] bytes = new byte[rangeTo-rangeFrom];
+        try {
+        	RandomAccessFile raf = new RandomAccessFile(new ClassPathResource("files/Joseph Ray - Room 1.5 (Extended Mix).mp3").getFile(),"r");
+        	raf.read(bytes, rangeFrom, rangeTo-rangeFrom);
+			 inputStream = new ClassPathResource("files/Joseph Ray - Room 1.5 (Extended Mix).mp3").getInputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        responseHeaders.setAccessControlExposeHeaders(Collections.singletonList("Content-Disposition"));
+        responseHeaders.set("Content-Disposition", "attachment; filename=" + "Joseph Ray - Room 1.5 (Extended Mix).mp3");
+        return new ResponseEntity<>(bytes, responseHeaders, HttpStatus.OK);
+    }
+    
+    @RequestMapping(path = "/api/files", method = {RequestMethod.HEAD}, produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> getWeight(){
+    	HttpHeaders headers = new HttpHeaders();
+    	File file = null;
+		try {
+			file = new ClassPathResource("files/Joseph Ray - Room 1.5 (Extended Mix).mp3").getFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	Long weight = file.length();
+    	headers.setAccessControlExposeHeaders(Arrays.asList("Content-Disposition","Content-Length"));
+        
+        headers.add("Content-Length", weight.toString());
+        headers.add("Content-Disposition", "attachment; filename=" + file.getName());
+    	return new ResponseEntity<>(weight.toString(), headers, HttpStatus.OK);
     }
 
 }
